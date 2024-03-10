2523c1bcf405704ffb19e5dd9463dfff196ee87b
Pratik Musmade <66913946+pratikmusmade@users.noreply.github.com>
2024-03-10 16:24:21 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import dotenv from 'dotenv'
-
-import connectDB from "./db/index.js";
-connectDB()
-
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
+import connectDB from "./db/index.js";
+import { app } from './app.js'
 
-// (async()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-//     } catch (error) {
-//         console.error("ERROR: ",error)
-//         throw err
-//     }
-// })()
+
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000,()=>{
+            console.log(` Server is running at port : ${process.env.PORT}`);
+        })
+    })
+    .catch((err)=>{
+        console.log('MongoDB connection failed FILENAME:index.js !!!! ',err);
+    })
